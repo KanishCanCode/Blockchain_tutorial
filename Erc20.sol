d8780bf5dfe87eb50d89ff5f73fe76b2a567e93b
KanishCanCode <147413494+KanishCanCode@users.noreply.github.com>
2023-12-02 19:46:50 +0530

    Update Erc20.sol

--- Erc20.sol
+++ Erc20.sol
@@ -1,12 +1,63 @@
-This contract includes the following methods:
+pragma solidity ^0.8.0;
 
-totalSupply(): Returns the total supply of tokens.
-balanceOf(address account): Returns the balance of tokens for a specific account.
-allowance(address owner, address spender): Returns the amount of tokens that an owner has allowed a spender to spend.
-transfer(address recipient, uint256 amount): Transfers a specified amount of tokens to a recipient.
-approve(address spender, uint256 amount): Approves a spender to spend a specified amount of tokens.
-transferFrom(address sender, address recipient, uint256 amount): Transfers a specified amount of tokens from a sender to a recipient.
-The contract also includes the following events:
+interface IERC20 {
+  function totalSupply() external view returns (uint256);
+  function balanceOf(address account) external view returns (uint256);
+  function allowance(address owner, address spender) external view returns (uint256);
+  function transfer(address recipient, uint256 amount) external returns (bool);
+  function approve(address spender, uint256 amount) external returns (bool);
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-Transfer(address indexed from, address indexed to, uint256 value): Emitted when tokens are transferred.
-Approval(address indexed owner, address indexed spender, uint256 value): Emitted when tokens are approved for spending.
+contract ERC20Basic is IERC20 {
+  string public constant name = "ERC20Basic";
+  string public constant symbol = "ERC";
+  uint8 public constant decimals = 18;
+
+  mapping(address => uint256) balances;
+  mapping(address => mapping (address => uint256)) allowed;
+  uint256 totalSupply_ = 10 ether;
+
+  constructor() {
+      balances[msg.sender] = totalSupply_;
+  }
+
+  function totalSupply() public override view returns (uint256) {
+      return totalSupply_;
+  }
+
+  function balanceOf(address tokenOwner) public override view returns (uint256) {
+      return balances[tokenOwner];
+  }
+
+  function transfer(address receiver, uint256 numTokens) public override returns (bool) {
+      require(numTokens <= balances[msg.sender]);
+      balances[msg.sender] = balances[msg.sender]-numTokens;
+      balances[receiver] = balances[receiver]+numTokens;
+      emit Transfer(msg.sender, receiver, numTokens);
+      return true;
+  }
+
+  function approve(address delegate, uint256 numTokens) public override returns (bool) {
+      allowed[msg.sender][delegate] = numTokens;
+      emit Approval(msg.sender, delegate, numTokens);
+      return true;
+  }
+
+  function allowance(address owner, address delegate) public override view returns (uint) {
+      return allowed[owner][delegate];
+  }
+
+  function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool) {
+      require(numTokens <= balances[owner]);
+      require(numTokens <= allowed[owner][msg.sender]);
+
+      balances[owner] = balances[owner]-numTokens;
+      allowed[owner][msg.sender] = allowed[owner][msg.sender]-numTokens;
+      balances[buyer] = balances[buyer]+numTokens;
+      emit Transfer(owner, buyer, numTokens);
+      return true;
+  }
+}
